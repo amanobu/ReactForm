2c7579e427cdbeba06185f7a07fb849871180a9a
ama_nobu <nqf33359@nifty.com>
2017-06-23 01:55:54 +0100

    親子に分解し、submitを押したときに子に入力されているデータをとれる様になった

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function getObjectStr(obj){
+    return JSON.stringify(obj);
+}
+
 class NameForm extends React.Component {
     constructor(props){
         super(props);
@@ -12,8 +16,12 @@ class NameForm extends React.Component {
             }
         };
         this.handleChange=this.handleChange.bind(this);
-        this.handleSubmit=this.handleSubmit.bind(this);
         this.handleBlur=this.handleBlur.bind(this);
+    }
+
+    getName(){
+        //console.log('NameFormComponent getName() called:'+getObjectStr(this.state.name));
+        return this.state.name;
     }
     
     handleChange(event){
@@ -29,24 +37,21 @@ class NameForm extends React.Component {
         default:
             break;
         }
-        
+
+        //Changeイベント毎に値を設定する必要がある模様
+        //でないと、キーを押した物が画面に反映されない
         this.setState({
             name: name
         });
     }
-    
-    handleSubmit(event){
-        alert('A name was submitted:' + this.state.name.lname + this.state.name.fname);
-        event.preventDefault();
-    }
 
     handleBlur(event){
-        console.log("blur:"+this.state.name.lname+this.state.name.fname);
+        console.log("blur:"+getObjectStr(this.state.name));
     }
 
     render(){
         return (
-                <form onSubmit={this.handleSubmit}>
+                <div>
                 <label>
                 姓:
                 <input type='text' name="lname" onChange={this.handleChange} onBlur={this.handleBlur}/>
@@ -55,15 +60,56 @@ class NameForm extends React.Component {
                 名:
                 <input type='text' name="fname" onChange={this.handleChange} onBlur={this.handleBlur}/>
                 </label>
+                </div>
+        );
+    }
+}
+
+class FormROOT extends React.Component {
+    constructor(props){
+        super(props);
+        //どうもこの記述が無いとstateが無いと怒られる...
+        this.state = {
+            //以下のデータは無くてもかまわないぽい...
+            /*
+            name:{
+                lname : '',
+                fname : ''
+            }
+            */
+        };
+        this.handleSubmit=this.handleSubmit.bind(this);
+    }
+    
+
+    getName(){
+        //console.log('FormROOTComponent getName() called(before setState):'+getObjectStr(this.state.name));
+        //一回目はのsetStateでは反映されない！
+        //もしかしたら、this.refs.～で直接とって来るのが良いのかもしれない
+        this.setState({
+            name : this.refs.NameFormComponent.getName()
+        });
+        //console.log('FormROOTComponent getName() called(after setState):'+getObjectStr(this.state.name));
+    }
+    
+    handleSubmit(event){
+        console.log(this.refs.NameFormComponent.getName());
+        //console.log('FormROOTComponent handleSubmit() called(before getName())');
+        this.getName();
+        //console.log('FormROOTComponent handleSubmit() called(after getName()):'+getObjectStr(this.state.name));
+        event.preventDefault();
+    }
+    render(){
+        return (
+                <form onSubmit={this.handleSubmit}>
+                <NameForm ref='NameFormComponent' />
                 <input type='submit' value='Submit' />
                 </form>
         );
     }
 }
 
-
-
 ReactDOM.render(
-        <NameForm />,
+        <FormROOT />,
     document.getElementById('root')
 );
